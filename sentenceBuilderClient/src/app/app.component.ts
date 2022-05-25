31365dd40f81cb4c3e3ecc95799fdf3267ef64c9
Hotdogwater52 <48760402+Hotdogwater52@users.noreply.github.com>
2022-05-25 18:43:16 +0200

    Updated Dependencies

--- sentenceBuilderClient/src/app/app.component.ts
+++ sentenceBuilderClient/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as express from 'express'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sentenceBuilderClient';
+  title = 'Sentence Builder';
 }
